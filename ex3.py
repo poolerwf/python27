6d3a6571b8a2ffeed27030d5d4acf04f1d8f0067
poolerwf <rwf.poole@gmail.com>
2014-07-15 06:11:45 +0200

    Initial commit 3

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# message
+print "I will now count my chickens:"
+
+# count hens and roosters
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# message
+print "Now I will count the eggs:"
+
+# count eggs
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# message
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# test math
+print 3.0 + 2 < 5.0 - 7.0
+
+# test math
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+
+# message
+print "Oh, that's why it's False."
+
+# message
+print "How about some more."
+
+# test math
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
